c5d78c4fc7a9ff35faa5d935a97345fc652f0a5a
JuliusUlee <61336116+Ulee-project@users.noreply.github.com>
2020-11-19 10:14:22 +0700

    Update userinfo.js

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -1,6 +1,6 @@
-const BaseEmbed = require("../.././modules/BaseEmbed");
+const BaseEmbed = require(".././modules/BaseEmbed");
 const { formatDate } = require("../../functions");
-const badges = require("../.././data/badges.json");
+const badges = require(".././data/badges.json");
 
 module.exports = {
   name: "userinfo",
